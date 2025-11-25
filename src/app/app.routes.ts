e2093642f19eb54029736cbab8408b5f546bd9a0
Cristhian Aragon <cristianaragon32a@gmail.com>
2025-11-25 16:16:05 -0600

    Se agrego componente standalone login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,16 @@ import { Routes } from "@angular/router";
 
 export const routes: Routes = [
     {
-        path: "",
-        redirectTo: "/home",
-        pathMatch: "full"
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
     },
     {
-        path: "home",
-        loadComponent: () => import("./modules/example-page/example-page.component").then((m) => m.ExamplePageComponent)
-
+        path: 'login',
+        loadComponent: () => import('./modules/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./modules/example-page/example-page.component').then((m) => m.ExamplePageComponent)
     }
 ];
