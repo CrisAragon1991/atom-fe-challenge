477da840f968b91d9daf655a370e8f48529b6d51
Cristhian Aragon <cristianaragon32a@gmail.com>
2025-11-26 09:40:01 -0600

    Se agregaron validaciones al componente de edit-component

--- src/app/modules/todo/edit-component/edit.component.ts
+++ src/app/modules/todo/edit-component/edit.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges } from '@angular/core';
+import { Todo } from '../../../models/todo';
+import { FormsModule } from '@angular/forms';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.scss'
 })
-export class EditComponent {
+export class EditComponent implements OnChanges {
+  @Input() todo!: Todo;
+  @Output() updated = new EventEmitter<Todo>();
+  @Output() closed = new EventEmitter<void>();
 
+  editTodo: Todo = {} as Todo;
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['todo'] && this.todo) {
+      this.editTodo = { ...this.todo };
+    }
+  }
+
+  save() {
+    this.updated.emit(this.editTodo);
+    this.close();
+  }
+
+  close() {
+    this.closed.emit();
+  }
 }
--- src/app/modules/todo/todo-component/todo.component.ts
+++ src/app/modules/todo/todo-component/todo.component.ts
@@ -1,19 +1,21 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodoService } from '../../../services/todo.service';
 import { Todo } from '../../../models/todo';
-
 import { CommonModule } from '@angular/common';
+import { EditComponent } from '../edit-component/edit.component';
 
 @Component({
   selector: 'app-todo',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EditComponent],
   templateUrl: './todo.component.html',
   styleUrl: './todo.component.scss'
 })
 export class TodoComponent implements OnInit {
   private todoService = inject(TodoService);
   todos = signal<Todo[]>([]);
+  showEditModal = signal(false);
+  selectedTodo = signal<Todo|null>(null);
 
   ngOnInit(): void {
     this.todoService.getTodos().subscribe({
@@ -25,5 +27,34 @@ export class TodoComponent implements OnInit {
         console.error('Error al cargar los todos', err);
       }
     });
+  }
+
+  openEditModal(todo: Todo) {
+    this.selectedTodo.set(todo);
+    this.showEditModal.set(true);
+  }
+
+  openNewTodoModal() {
+    this.selectedTodo.set({
+      id: '',
+      name: '',
+      description: '',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      stateId: '1',
+    });
+    this.showEditModal.set(true);
+  }
+
+  closeEditModal() {
+    this.showEditModal.set(false);
+    this.selectedTodo.set(null);
+  }
+
+  updateTodo(edited: Todo) {
+    this.todos.set(
+      this.todos().map(t => t.id === edited.id ? { ...t, ...edited } : t)
+    );
+    this.closeEditModal();
   }
 }
